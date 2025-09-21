08a98aea0e44ddb7b39011ba6f02e1530823ccbd
maciej marosz <webaby@MacBook-Air-macmaro.local>
2025-09-21 20:42:00 +0200

    change privacy-policy-en

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,24 +2,41 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { PrivacyPolicyEnComponent } from '../../pages/privacy-policy-en/privacy-policy-en.component';
+import { ProductDialogComponent } from '../../shared/product-dialog/product-dialog.component';
+import { PRIVACY_POLICY_EN } from '../..//pages/privacy-policy-en/privacy-policy-text.en';
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
-    imports: [CommonModule, MatDialogModule, MatButtonModule, PrivacyPolicyEnComponent],
+    imports: [CommonModule, MatDialogModule, MatButtonModule],
     standalone: true
 })
 export class FooterComponent{ 
     year=new Date().getFullYear();
     private dialog = inject(MatDialog);
 
-  openPrivacyPolicy() {
-    this.dialog.open(PrivacyPolicyEnComponent, {
-      width: '800 px',
-      maxWidth: '95vw',
-      autoFocus: false,
-      restoreFocus: true,
-    });
-  }
+
+
+
+
+openPrivacyPolicy() {
+  this.dialog.open(ProductDialogComponent, {
+    data: {
+      title: 'Privacy policy',
+      desc: PRIVACY_POLICY_EN,   // tutaj idzie cały tekst
+      imageUrl: '',
+      slug: 'privacy-policy',
+      showImage: false,
+      showOpenLinkButton: false,
+      showDetailsButton: false,
+      showQr: false,
+      showCloseButton: true,
+    },
+    width: '800px',
+    maxWidth: '95vw',
+    maxHeight: '90vh',
+    autoFocus: false,
+    restoreFocus: true,
+  });
+}
 }
