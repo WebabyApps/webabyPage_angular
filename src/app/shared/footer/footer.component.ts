7407fe4e50e3ed48379819566d7089af8a81d7af
maciej marosz <webaby@MacBook-Air-macmaro.local>
2025-09-26 10:52:55 +0200

    privacy policy router import added

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { RouterModule } from '@angular/router';  
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule], 
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
